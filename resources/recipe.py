ac36ce3b36d6994900684dfcd75202f1fae89acc
oreeee <yeongcheun_lee@hotmail.com>
2021-07-11 21:51:33 +0800

    finished chp2

--- resources/recipe.py
+++ resources/recipe.py
@@ -5,12 +5,11 @@ from models.recipe import Recipe, recipe_list
 
 
 class RecipeListResource(Resource):
-
     def get(self):
         data = []
         for recipe in recipe_list:
             if recipe.is_publish is True:
-                data.append(recipe)
+                data.append(recipe.data)
 
         return {"data": data}, HTTPStatus.OK
 
@@ -22,12 +21,11 @@ class RecipeListResource(Resource):
                         cook_time=data["cook_time"],
                         directions=data["directions"]
                         )
-        recipe_list.append(data)
+        recipe_list.append(recipe)
         return recipe.data, HTTPStatus.CREATED
 
 
 class RecipeResource(Resource):
-
     def get(self, recipe_id):
         recipe = next((recipe for recipe in recipe_list
                        if recipe.id == recipe_id and recipe.is_publish == True), None)
@@ -46,4 +44,21 @@ class RecipeResource(Resource):
         recipe.cook_time = data["cook_time"]
         recipe.directions = data["direction"]
         return recipe.data, HTTPStatus.OK
+
+
+class RecipePublishResource(Resource):
+    def put(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+        recipe.is_publish = True
+        return {}, HTTPStatus.NO_CONTENT
+
+    def delete(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+        recipe.is_publish = False
+        return {}, HTTPStatus.NO_CONTENT
+
 
